6d731fc5a192c0943390fda9db3f0b2892ccbd64
kiteh <kiteho8962@naver.com>
2021-04-26 18:09:48 +0900

    Yeon 커밋01

--- src/pages/Youtuber.js
+++ src/pages/Youtuber.js
@@ -1,11 +1,11 @@
 import React from 'react';
 // nav에서 유튜버를 누르면 보이는 전체 컴포넌트
 const Youtuber = () => {
-    return (
-        <div>
-            <h1>유튜버 게시판</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>유튜버 게시판</h1>
+    </div>
+  );
 };
 
 export default Youtuber;
