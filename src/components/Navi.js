6da86570ee5ee74595c7515bc290b674b7ee2d7d
kiteh <kiteho8962@naver.com>
2021-04-26 17:01:20 +0900

    Y통합완료2

--- src/components/Navi.js
+++ src/components/Navi.js
@@ -8,14 +8,29 @@ const Navi = () => {
     <>
       <div className='nav'>
         <ul className='nav-pills' defaultValue='/home'>
-          <li className='nav-link' href="/home">
-            <Link to="/">메인</Link>
+          <li className='nav-link' href='/home'>
+            <Link to='/'>메인</Link>
           </li>
-          <li className='nav-link'> <Link to="/Youtuber">유튜버</Link> </li>
-          <li className='nav-link'> <Link to="/Editer">편집자</Link> </li>
-          <li className='nav-link'> <Link to="/Thumbnailer">썸네일러</Link> </li>
-          <li className='nav-link'> <Link to="/Winwin">윈윈</Link> </li>
-          <li className='nav-link'> <Link to="/Help">고객센터</Link> </li>
+          <li className='nav-link'>
+            {' '}
+            <Link to='/Youtuber'>유튜버</Link>{' '}
+          </li>
+          <li className='nav-link'>
+            {' '}
+            <Link to='/Editer'>편집자</Link>{' '}
+          </li>
+          <li className='nav-link'>
+            {' '}
+            <Link to='/Thumbnailer'>썸네일러</Link>{' '}
+          </li>
+          <li className='nav-link'>
+            {' '}
+            <Link to='/Winwin'>윈윈</Link>{' '}
+          </li>
+          <li className='nav-link'>
+            {' '}
+            <Link to='/Help'>고객센터</Link>{' '}
+          </li>
         </ul>
       </div>
     </>
