222c2b5390a6fb225d2f2218a518530cb4d35117
Jinseob <rlawlstjq343@naver.com>
2021-04-27 16:29:54 +0900

    0427 진섭 세번째 커밋

--- src/components/SignButton.js
+++ src/components/SignButton.js
@@ -1,5 +1,5 @@
 import React, { Component, useState } from 'react';
-import SignUp from './SignUp';
+import SignUp from './LoginModal';
 
 const SignButton = () => {
   const [modalOpen, setModalOpen] = useState(true);
@@ -22,7 +22,7 @@ const SignButton = () => {
       <button onClick={openModal}>로그인/회원가입</button>
       {/* header 부분에 텍스트를 입력 */}
       <SignUp open={modalOpen} close={closeModal} header="Modal heading">
-        {/* SignUp.js <main> {props.children} </main>에 내용이 입력된다. */}
+        {/* LoginModal.js <main> {props.children} </main>에 내용이 입력된다. */}
         회원가입회원가입회원가입 커밋시험용1입니다
       </SignUp>
     </>
