41b7d5059ede3fd24dd651b851ada3de779b1bd8
kiteh <kiteho8962@naver.com>
2021-04-27 10:23:52 +0900

    Yeon 커밋0427_01

--- src/components/columns.js
+++ src/components/columns.js
@@ -3,23 +3,19 @@ import { format } from 'date-fns';
 export const COLUMNS = [
   {
     Header: 'Id',
-    Footer: 'Id',
     accessor: 'id',
     disableFilters: true,
   },
   {
     Header: 'First Name',
-    Footer: 'First Name',
     accessor: 'first_name',
   },
   {
     Header: 'Last Name',
-    Footer: 'Last Name',
     accessor: 'last_name',
   },
   {
     Header: 'Date of Birth',
-    Footer: 'Date of Birth',
     accessor: 'date_of_birth',
     Cell: ({ value }) => {
       return format(new Date(value), 'yyyy/MM/dd');
@@ -27,12 +23,10 @@ export const COLUMNS = [
   },
   {
     Header: 'Country',
-    Footer: 'Country',
     accessor: 'country',
   },
   {
     Header: 'Phone',
-    Footer: 'Phone',
     accessor: 'phone',
   },
 ];
