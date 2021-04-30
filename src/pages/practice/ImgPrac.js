2b757e87c67188a3680347bd3be93312559e459a
Jongmin <dbswww@naver.com>
2021-05-01 01:09:58 +0900

    imgUploadTest

--- src/pages/practice/ImgPrac.js
+++ src/pages/practice/ImgPrac.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
-import ReactQuill from "react-quill";
-let quillObj;
+import ReactQuill, { Quill } from "react-quill";
+import ImgApiService from "./ImgApiService";
+import "react-quill/dist/quill.snow.css";
+import "../../components/scss/QuillComponents.scss";
+let Image = Quill.import("formats/image");
+Image.className = "custom-class-to-image";
+Quill.register(Image, true);
 
-const ImgPrac = (props) => {
-  const [Description, setDescription] = useState();
+const ImgPrac = () => {
   const imageHandler = () => {
+    let self = this;
     const input = document.createElement("input");
 
     input.setAttribute("type", "file");
@@ -15,50 +20,80 @@ const ImgPrac = (props) => {
       const file = input.files[0];
       const formData = new FormData();
 
-      formData.append("image", file);
-
-      // Save current cursor state
-      const range = this.quill.getSelection(true);
-
-      // Insert temporary loading placeholder image
-      this.quill.insertEmbed(range.index, "image", `${window.location.origin}/images/loaders/placeholder.gif`);
-
-      // Move cursor to right side of image (easier to continue typing)
-      this.quill.setSelection(range.index + 1);
-
-      const res = await apiPostNewsImage(formData); // API post, returns image location as string e.g. 'http://www.example.com/images/foo.png'
-
-      // Remove placeholder image
-      this.quill.deleteText(range.index, 1);
-
-      // Insert uploaded image
-      this.quill.insertEmbed(range.index, "image", res.body.image);
+      formData.append("file", file);
+      const config = {
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+      };
+      const res = await ImgApiService.addImgs(formData, config)
+        .then((response) => {
+          if (response.data.error == "false" || response.data.error == false) {
+            if (response.data.status == 200 && response.data.message == "Image uploaded successfully") {
+              const range = self.quill.getSelection(true);
+              // Insert temporary loading placeholder image
+              // this.quill.insertEmbed(range.index, 'image', `${window.location.origin}/images/loaders/placeholder.gif`);
+              // Move cursor to right side of image (easier to continue typing)
+              self.quill.setSelection(range.index + 1);
+              // Remove placeholder image
+              self.quill.deleteText(range.index, 1);
+              // Insert uploaded image
+              self.quill.insertEmbed(range.index, "image", response.data.data[0].imageURL);
+            }
+          }
+          console.log(response);
+        })
+        .catch((error) => {
+          alert(error);
+        });
     };
   };
+  const modules = {
+    toolbar: {
+      container: [
+        [{ header: [1, 2, false] }],
+        ["bold", "italic", "underline", "strike", "blockquote"],
+        [{ list: "ordered" }, { list: "bullet" }, { indent: "-1" }, { indent: "+1" }],
+        ["link", "image", "video"],
+        [{ align: [] }, { color: [] }, { background: [] }],
+        ["clean"],
+      ],
+      handlers: { image: imageHandler },
+    },
+  };
+
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+    "align",
+    "video",
+    "color",
+    "background",
+  ];
+
+  const [data, setData] = useState("");
   return (
-    <div>
+    <div style={{ height: "650px" }}>
       <ReactQuill
-        value={Description}
-        modules={{
-          toolbar: {
-            container: [
-              [{ header: [1, 2, 3, 4, 5, 6, false] }],
-              ["bold", "italic", "underline"],
-              [{ list: "ordered" }, { list: "bullet" }],
-              [{ align: [] }],
-              ["link", "image"],
-              ["clean"],
-              [{ color: [] }],
-            ],
-            handlers: {
-              image: imageHandler,
-            },
-          },
-          table: true,
-        }}
+        style={{ height: "600px" }}
+        theme='snow'
+        modules={modules}
+        formats={formats}
         placeholder='Add a description of your event'
-        onChange={(content, delta, source, editor) => setDescription(content, editor)}
         id='txtDescription'
+        value={data || ""}
+        onChange={(content, delta, source, editor) => {
+          setData(editor.getHTML());
+        }}
       />
     </div>
   );
