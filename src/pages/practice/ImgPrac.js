12b7e56448af1d46a200e0be55657eb4b35b2c08
Jongmin <dbswww@naver.com>
2021-05-02 01:43:53 +0900

    Finally, figured out what was wrong with imgs..

--- src/pages/practice/ImgPrac.js
+++ src/pages/practice/ImgPrac.js
@@ -8,14 +8,8 @@ Image.className = "custom-class-to-image";
 Quill.register(Image, true);
 let quill;
 const ImgPrac = () => {
-  //클래스형 으론 써본적 없지만
-  //React Quill을 함수형으로 쓰기위해서는 Quill 객체를 선언해서 그걸 div안에 넣어줘야한다.
-  //react에서 div에 접근할때 바로 접근하면 rendering이 되기전에 해당 div를 접근하려하기때문에
-  //render가 끝난뒤에 해당 div의 id값을 잡을수 있게끔 useEffect로 접근한다.
-
   //imageHandler같은 함수는 useCallback로 감싸서 렌더링될때 한번만 실행되게 해야한다.
   const imageHandler = useCallback(() => {
-    console.log(quill.getSelection(true));
     const input = document.createElement("input");
 
     input.setAttribute("type", "file");
@@ -34,15 +28,18 @@ const ImgPrac = () => {
       const res = await ImgApiService.addImgs(formData, config)
         .then((response) => {
           if (response.status === 200) {
-            const range = quill.getSelection(true) === null ? quill.getSelection(true) : 0;
+            const range = quill.getSelection(true) !== null ? quill.getSelection(true) : 0;
             // 로딩 이미지를 잠깐 넣는다. 실행되는 함수 ex)
-            // this.quill.insertEmbed(range.index, 'image', `${window.location.origin}/images/loaders/placeholder.gif`);
+            quill.insertEmbed(range.index, "image", `http://localhost:8888/imgs/placeholder.gif`);
             // 커서를 이미지 바로 뒤로 움직여서 이미지 넣은 후 글쓰기 쉽게 해준다
             quill.setSelection(range.index + 1);
             // 로딩 이미지를 지운다
             quill.deleteText(range.index, 1);
             // 업로드된 이미지를 서버에서 받아서 넣어준다
-            quill.insertEmbed(range.index, "image", response.data[0].uploadPath);
+            console.log(response.data[0].uploadPath);
+            quill.insertEmbed(range.index, "image", "http://localhost:8888/imgs/test.png");
+            // quill.insertEmbed(range.index, "image", response.data[0].uploadPath);
+            // 서버가 켜있으므로 서버의 주소 정적 폴더 밑 파일을 가져올수있다!
           }
         })
         .catch((error) => {
@@ -90,31 +87,49 @@ const ImgPrac = () => {
     []
   );
   const [data, setData] = useState();
-  const onChange = (content, delta, source, editor) => {
-    setData(editor.getHTML());
-    console.log(editor.getHTML());
-  };
+
+  //클래스형 으론 써본적 없지만
+  //React Quill을 함수형으로 쓰기위해서는 Quill 객체를 선언해서 그걸 div안에 넣어줘야한다.
+  //react에서 div에 접근할때 바로 접근하면 rendering이 되기전에 해당 div를 접근하려하기때문에
+  //render가 끝난뒤에 해당 div의 id값을 잡을수 있게끔 useEffect로 접근한다.
   useEffect(() => {
     let container = document.getElementById("ReactQuill");
     quill = new Quill(container, {
       modules: modules,
       formats: formats,
       theme: "snow",
-      placeholder: "내용 입력..",
-      value: data,
-      onChange: onChange,
+      placeholder: "내용입력",
+      value: "hi",
     });
-    console.log(quill);
-  });
+    quill.on("text-change", (delta, oldDelta, source) => {
+      setData(quill.root.innerHTML);
+    });
+  }, [setData, modules, formats]);
 
   return (
     <div>
-      <div style={{ height: "650px" }} id='ReactQuill'></div>
       <div>
-        <button onClick={() => console.log(data)}>클릭!</button>
+        <div style={{ height: "350px" }} id='ReactQuill'></div>
+      </div>
+      <div>
+        <div>
+          <button onClick={() => console.log(data)}>확인</button>
+        </div>
       </div>
     </div>
   );
 };
 
 export default ImgPrac;
+// {/* <ReactQuill
+// style={{ height: "600px" }}
+// theme='snow'
+// modules={modules}
+// formats={formats}
+// placeholder='Add a description of your event'
+// id='ReactQuill'
+// value={data || ""}
+// onChange={(content, delta, source, editor) => {
+//   setData(editor.getHTML());
+// }}
+// /> */}
