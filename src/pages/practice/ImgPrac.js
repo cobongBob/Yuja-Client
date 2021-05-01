eb3cc6f33d4213adc25d0d373476b4ee2a2d23ee
Jongmin <dbswww@naver.com>
2021-05-01 23:32:10 +0900

    Study React-Quill

--- src/pages/practice/ImgPrac.js
+++ src/pages/practice/ImgPrac.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import ReactQuill, { Quill } from "react-quill";
 import ImgApiService from "./ImgApiService";
 import "react-quill/dist/quill.snow.css";
@@ -6,20 +6,25 @@ import "../../components/scss/QuillComponents.scss";
 let Image = Quill.import("formats/image");
 Image.className = "custom-class-to-image";
 Quill.register(Image, true);
-
+let quill;
 const ImgPrac = () => {
-  const imageHandler = () => {
-    let self = this;
+  useEffect(() => {
+    let container = document.getElementById("ReactQuill");
+    quill = new Quill(container);
+  }, []);
+
+  //imageHandler같은 함수는 useCallback로 감싸서 렌더링될때 한번만 실행되게 해야한다.
+  const imageHandler = useCallback(() => {
+    console.log(quill.getSelection(true));
     const input = document.createElement("input");
 
     input.setAttribute("type", "file");
-    input.setAttribute("accept", "image/*");
+    input.setAttribute("accept", "image/png, image/jpeg, image/gif");
     input.click();
 
     input.onchange = async () => {
       const file = input.files[0];
       const formData = new FormData();
-
       formData.append("file", file);
       const config = {
         headers: {
@@ -28,59 +33,63 @@ const ImgPrac = () => {
       };
       const res = await ImgApiService.addImgs(formData, config)
         .then((response) => {
-          if (response.data.error == "false" || response.data.error == false) {
-            if (response.data.status == 200 && response.data.message == "Image uploaded successfully") {
-              const range = self.quill.getSelection(true);
-              // Insert temporary loading placeholder image
-              // this.quill.insertEmbed(range.index, 'image', `${window.location.origin}/images/loaders/placeholder.gif`);
-              // Move cursor to right side of image (easier to continue typing)
-              self.quill.setSelection(range.index + 1);
-              // Remove placeholder image
-              self.quill.deleteText(range.index, 1);
-              // Insert uploaded image
-              self.quill.insertEmbed(range.index, "image", response.data.data[0].imageURL);
-            }
+          if (response.status === 200) {
+            const range = quill.getSelection(true) === null ? quill.getSelection(true) : 0;
+            // 로딩 이미지를 잠깐 넣는다. 실행되는 함수 ex)
+            // this.quill.insertEmbed(range.index, 'image', `${window.location.origin}/images/loaders/placeholder.gif`);
+            // 커서를 이미지 바로 뒤로 움직여서 이미지 넣은 후 글쓰기 쉽게 해준다
+            quill.setSelection(range.index + 1);
+            // 로딩 이미지를 지운다
+            quill.deleteText(range.index, 1);
+            // 업로드된 이미지를 서버에서 받아서 넣어준다
+            quill.insertEmbed(range.index, "image", response.data[0].uploadPath);
           }
-          console.log(response);
         })
         .catch((error) => {
           alert(error);
         });
     };
-  };
-  const modules = {
-    toolbar: {
-      container: [
-        [{ header: [1, 2, false] }],
-        ["bold", "italic", "underline", "strike", "blockquote"],
-        [{ list: "ordered" }, { list: "bullet" }, { indent: "-1" }, { indent: "+1" }],
-        ["link", "image", "video"],
-        [{ align: [] }, { color: [] }, { background: [] }],
-        ["clean"],
-      ],
-      handlers: { image: imageHandler },
-    },
-  };
+  }, []);
 
-  const formats = [
-    "header",
-    "bold",
-    "italic",
-    "underline",
-    "strike",
-    "blockquote",
-    "list",
-    "bullet",
-    "indent",
-    "link",
-    "image",
-    "align",
-    "video",
-    "color",
-    "background",
-  ];
+  //modules,format같은 값은 useMemo로 감싸서 렌더링될때 한번만 실행되게 해야한다.
+  const modules = useMemo(
+    () => ({
+      toolbar: {
+        container: [
+          [{ header: [1, 2, false] }],
+          ["bold", "italic", "underline", "strike", "blockquote"],
+          [{ list: "ordered" }, { list: "bullet" }, { indent: "-1" }, { indent: "+1" }],
+          ["link", "image", "video"],
+          [{ align: [] }, { color: [] }, { background: [] }],
+          ["clean"],
+        ],
+        handlers: { image: imageHandler },
+      },
+    }),
+    [imageHandler]
+  );
 
-  const [data, setData] = useState("");
+  const formats = useMemo(
+    () => [
+      "header",
+      "bold",
+      "italic",
+      "underline",
+      "strike",
+      "blockquote",
+      "list",
+      "bullet",
+      "indent",
+      "link",
+      "image",
+      "align",
+      "video",
+      "color",
+      "background",
+    ],
+    []
+  );
+  const [data, setData] = useState();
   return (
     <div style={{ height: "650px" }}>
       <ReactQuill
@@ -89,7 +98,7 @@ const ImgPrac = () => {
         modules={modules}
         formats={formats}
         placeholder='Add a description of your event'
-        id='txtDescription'
+        id='ReactQuill'
         value={data || ""}
         onChange={(content, delta, source, editor) => {
           setData(editor.getHTML());
