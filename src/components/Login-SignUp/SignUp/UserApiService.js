2e02b6fa55c3911b9e5ecee6719d0988eacaf114
jinseob kim <rlawlstjq343@naver.com>
2021-05-04 19:05:28 +0900

    0504 진섭 07:05 프로필사진 업로드 버전

--- src/components/Login-SignUp/SignUp/UserApiService.js
+++ src/components/Login-SignUp/SignUp/UserApiService.js
@@ -3,8 +3,17 @@ import axios from 'axios';
 const USER_API_BASE_URL = 'http://localhost:8888/api/user';
 
 class UserApiService {
+
   async addUser(data) {
     return await axios.post(USER_API_BASE_URL, data);
+  }
+
+  async addProfileImg(data, config) {
+    return await axios.post(
+      USER_API_BASE_URL + '/profile',
+      data,
+      config
+    );
   }
 }
 
