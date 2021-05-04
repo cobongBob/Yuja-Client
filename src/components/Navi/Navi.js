d6d939c4473aec3e5e71147306bef6bd263ab68e
ChoiJaeYeon <jaey1229@gmail.com>
2021-05-04 19:24:07 +0900

    - 0504 navi 반응형 적용중

--- src/components/Navi/Navi.js
+++ src/components/Navi/Navi.js
@@ -3,36 +3,38 @@ import './Navi.scss';
 import { Link } from 'react-router-dom';
 import './SignButton.scss';
 import LoginModal from '../Login-SignUp/Login/LoginModal';
+import { FiMenu } from 'react-icons/fi';
 
 const Navi = () => {
   return (
-    <>
-      <div className='nav'>
-        <ul className='nav-pills' defaultValue='/'>
-          <Link to='/'>
-            <li className='nav-link'>메인</li>
-          </Link>
-          <Link to='/Youtuber'>
-            <li className='nav-link'>유튜버</li>
-          </Link>
-          <Link to='/Editer'>
-            <li className='nav-link'>편집자</li>
-          </Link>
-          <Link to='/Thumbnailer'>
-            <li className='nav-link'>썸네일러</li>
-          </Link>
-          <Link to='/Winwin'>
-            <li className='nav-link'>윈윈</li>
-          </Link>
-          <Link to='/Help'>
-            <li className='nav-link'>고객센터</li>
-          </Link>
-          <div className='nav-login'>
-            <LoginModal />
-          </div>
-        </ul>
+    <div className='nav'>
+      <ul className='nav-pills' defaultValue='/'>
+        <Link to='/'>
+          <li className='nav-link'>메인</li>
+        </Link>
+        <Link to='/Youtuber'>
+          <li className='nav-link'>유튜버</li>
+        </Link>
+        <Link to='/Editer'>
+          <li className='nav-link'>편집자</li>
+        </Link>
+        <Link to='/Thumbnailer'>
+          <li className='nav-link'>썸네일러</li>
+        </Link>
+        <Link to='/Winwin'>
+          <li className='nav-link'>윈윈</li>
+        </Link>
+        <Link to='/Help'>
+          <li className='nav-link'>고객센터</li>
+        </Link>
+        <li className='nav-login'>
+          <LoginModal />
+        </li>
+      </ul>
+      <div>
+        <FiMenu className='menu-icon'></FiMenu>
       </div>
-    </>
+    </div>
   );
 };
 
