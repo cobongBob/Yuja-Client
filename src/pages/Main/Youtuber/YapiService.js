9418eeedcfe3d6b45faa3dc031e43922d4acfa31
Jongmin <dbswww@naver.com>
2021-05-06 00:18:34 +0900

    security trying...

--- src/pages/Main/Youtuber/YapiService.js
+++ src/pages/Main/Youtuber/YapiService.js
@@ -1,28 +1,26 @@
-import axios from "axios";
-
-const USER_API_BASE_URL = "http://localhost:8888/api/";
+import instance from "../../../AxiosConfig.js";
 const BoardType = 1;
 
 class YapiService {
-  async addBoards(dita) {
-    return await axios.post(USER_API_BASE_URL + BoardType + "/board", dita);
+  async addBoards(data) {
+    return await instance({ url: BoardType + "/board", method: "post", data: data });
   }
 
   async fetchBoards() {
-    return await axios.get(USER_API_BASE_URL + BoardType + "/board");
+    return await instance({ url: BoardType + "/board", method: "get" });
   }
 
   // 상세보기 1개만
   async fetchBoard(board_id) {
-    return await axios.get(USER_API_BASE_URL + BoardType + "/board/" + board_id);
+    return await instance({ url: BoardType + "/board/" + board_id, method: "get" });
   }
 
   async modifyBoard(board_id, data) {
-    return await axios.put(USER_API_BASE_URL + BoardType + "/board/" + board_id, data);
+    return await instance.put({ url: BoardType + "/board", method: "put", data: data });
   }
 
   async deleteBoard(board_id) {
-    return await axios.delete(USER_API_BASE_URL + BoardType + "/board/" + board_id);
+    return await instance.delete({ url: BoardType + "/board/" + board_id, method: "delete" });
   }
 }
 
