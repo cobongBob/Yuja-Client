80d2aeb9d9abbace45fa4dddec3e5a3a193125cf
ChoiJaeYeon <jaey1229@gmail.com>
2021-05-13 18:34:18 +0900

    0513 18:34pm

--- src/components/Navi/Navi.js
+++ src/components/Navi/Navi.js
@@ -1,44 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Navi.scss';
-import { Link, useHistory, useLocation } from 'react-router-dom';
+import { Link, useHistory, useLocation, useRouteMatch } from 'react-router-dom';
 import './SignButton.scss';
 import LoginModal from '../Login-SignUp/Login/LoginModal';
 import { FiMenu } from 'react-icons/fi';
 
 const Navi = () => {
-  {
-    /*
-  const history = useHistory();
-  const current = useHistory().location.pathname;
+  const current = useHistory().location.state;
+  const pathname = useLocation();
+  const [match, setMatch] = useState(current);
 
-  const handelClick = (e) => {
-    if (current) {
-      e.preventDefault();
-    }
-  };
-*/
-  }
+  const clickLink = () => {};
 
   return (
     <div className='nav'>
       <ul className='nav-pills' defaultValue='/'>
         <Link to='/'>
-          <li className='nav-link'>메인</li>
+          <li
+            className={current === pathname ? 'nav-link-disabled' : 'nav-link'}
+          >
+            메인
+          </li>
         </Link>
         <Link to='/Youtuber'>
-          <li className='nav-link'>유튜버</li>
+          <li
+            className={current === pathname ? 'nav-link-disabled' : 'nav-link'}
+          >
+            유튜버
+          </li>
         </Link>
         <Link to='/Editer'>
-          <li className='nav-link'>편집자</li>
+          <li
+            className={current === pathname ? 'nav-link-disabled' : 'nav-link'}
+          >
+            편집자
+          </li>
         </Link>
         <Link to='/Thumbnailer'>
-          <li className='nav-link'>썸네일러</li>
+          <li
+            className={current === pathname ? 'nav-link-disabled' : 'nav-link'}
+          >
+            썸네일러
+          </li>
         </Link>
         <Link to='/Winwin'>
-          <li className='nav-link'>윈윈</li>
+          <li
+            className={current === pathname ? 'nav-link-disabled' : 'nav-link'}
+          >
+            윈윈
+          </li>
         </Link>
         <Link to='/Help'>
-          <li className='nav-link'>고객센터</li>
+          <li
+            className={current === pathname ? 'nav-link-disabled' : 'nav-link'}
+          >
+            고객센터
+          </li>
         </Link>
         <li className='nav-login'>
           <LoginModal />
