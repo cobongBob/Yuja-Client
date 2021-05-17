44ccdf74d78500ba82bf67eda1bf327bb66fefd0
Jongmin <dbswww@naver.com>
2021-05-17 10:08:45 +0900

    board_code added to axios url

--- src/apiService/winBoardApiService.js
+++ src/apiService/winBoardApiService.js
@@ -30,8 +30,27 @@ export const getWinBoards = async (board_type) => {
   });
 };
 export const getWinOneBoard = async (board_id, board_type) => {
-  if (board_type === "Winwin") {
-    board_code = 4;
+  switch (board_type) {
+    case "Youtuber":
+      board_code = 1;
+      break;
+    case "Editor":
+      board_code = 2;
+      break;
+    case "Thumb":
+      board_code = 3;
+      break;
+    case "Winwin":
+      board_code = 4;
+      break;
+    case "Collabo":
+      board_code = 5;
+      break;
+    case "Notice":
+      board_code = 6;
+      break;
+    default:
+      board_code = 0;
   }
   return await instance({
     url: board_code + "/board/" + board_id,
@@ -39,8 +58,27 @@ export const getWinOneBoard = async (board_id, board_type) => {
   });
 };
 export const insertWinBoard = async (data, board_type) => {
-  if (board_type === "Winwin") {
-    board_code = 4;
+  switch (board_type) {
+    case "Youtuber":
+      board_code = 1;
+      break;
+    case "Editor":
+      board_code = 2;
+      break;
+    case "Thumb":
+      board_code = 3;
+      break;
+    case "Winwin":
+      board_code = 4;
+      break;
+    case "Collabo":
+      board_code = 5;
+      break;
+    case "Notice":
+      board_code = 6;
+      break;
+    default:
+      board_code = 0;
   }
   return await instance({
     url: board_code + "/board",
@@ -49,8 +87,27 @@ export const insertWinBoard = async (data, board_type) => {
   });
 };
 export const modifyWinBoard = async (board_id, data, board_type) => {
-  if (board_type === "Winwin") {
-    board_code = 4;
+  switch (board_type) {
+    case "Youtuber":
+      board_code = 1;
+      break;
+    case "Editor":
+      board_code = 2;
+      break;
+    case "Thumb":
+      board_code = 3;
+      break;
+    case "Winwin":
+      board_code = 4;
+      break;
+    case "Collabo":
+      board_code = 5;
+      break;
+    case "Notice":
+      board_code = 6;
+      break;
+    default:
+      board_code = 0;
   }
   return await instance({
     url: board_code + "/board/" + board_id,
@@ -59,8 +116,27 @@ export const modifyWinBoard = async (board_id, data, board_type) => {
   });
 };
 export const deleteWinBoard = async (board_id, board_type) => {
-  if (board_type === "Winwin") {
-    board_code = 4;
+  switch (board_type) {
+    case "Youtuber":
+      board_code = 1;
+      break;
+    case "Editor":
+      board_code = 2;
+      break;
+    case "Thumb":
+      board_code = 3;
+      break;
+    case "Winwin":
+      board_code = 4;
+      break;
+    case "Collabo":
+      board_code = 5;
+      break;
+    case "Notice":
+      board_code = 6;
+      break;
+    default:
+      board_code = 0;
   }
   return await instance({
     url: board_code + "/board/" + board_id,
