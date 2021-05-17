b85621c63a33894c359b056c6617799e00a8f4ad
ChoiJaeYeon <jaey1229@gmail.com>
2021-05-17 15:43:47 +0900

    0517 15:43PM

--- src/components/Loading/Loader.js
+++ src/components/Loading/Loader.js
@@ -1,11 +1,23 @@
-import React from "react";
-import ReactLoading from "react-loading";
+import React from 'react';
+import ReactLoading from 'react-loading';
 
 const Loader = ({ type, color }) => {
   return (
     <div class='contentWrap'>
-      <div style={{ position: "fixed", top: "60%", left: "50%", transform: "translate(-50%, -50%)" }}>
-        <ReactLoading type={"spinningBubbles"} color={color} height={"125px"} width={"125px"} />{" "}
+      <div
+        style={{
+          position: 'fixed',
+          top: '60%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+        }}
+      >
+        <ReactLoading
+          type={'cylon'}
+          color={color}
+          height={'125px'}
+          width={'125px'}
+        />{' '}
       </div>
     </div>
   );
