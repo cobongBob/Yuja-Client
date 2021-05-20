b62671247d23515528134d970176851529c9ebab
kiteh <kiteho8962@naver.com>
2021-05-20 22:15:06 +0900

    Yeon 채팅 부들부들 유아이 신난당~

--- src/pages/Main/components/Chat/Chat.js
+++ src/pages/Main/components/Chat/Chat.js
@@ -1,15 +1,25 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import "./Chat.scss";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import './Chat.scss';
+import { AiFillWechat } from 'react-icons/ai';
+import { GiLoveLetter } from 'react-icons/gi';
 
 const Chat = () => {
   const { userData } = useSelector((state) => state.loginReducer);
 
   return userData.id ? (
     <div className='chat_body'>
+      <div className='ChatIconsWrapper'>
+        <h1>
+          <AiFillWechat className='ChatIcons'></AiFillWechat>유자톡
+        </h1>
+      </div>
       <div className='chat_frame_wrapper'>
         <div className='chat_frame'>
-          <iframe src='http://localhost:8888/rooms' title='YujaChat'></iframe>
+          <iframe
+            className='chattingFrame'
+            src='http://localhost:8888/rooms'
+            title='YujaChat'></iframe>
         </div>
       </div>
     </div>
