d44ba19db3a1c160a2225cc20c73bd9738b4a7dc
kiteh <kiteho8962@naver.com>
2021-05-24 17:44:54 +0900

    Yeon 권한 설정 준비

--- src/apiService/ReportApiService.js
+++ src/apiService/ReportApiService.js
@@ -1,16 +1,17 @@
-import instance from "../AxiosConfig.js";
+import instance from '../AxiosConfig.js';
 let board_code = 8;
 
 export const addReport = async (data) => {
   return await instance({
-    url: board_code + "/board",
-    method: "post",
+    url: board_code + '/board',
+    method: 'post',
     data: data,
   });
 };
+
 export const fetchReports = async () => {
   return await instance({
-    url: board_code + "/board",
-    method: "get",
+    url: board_code + '/board',
+    method: 'get',
   });
 };
