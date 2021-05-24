56e678a3ee8ca6baa1d629f6826a3a059661817f
Iohc <chnew12@naver.com>
2021-05-24 21:34:56 +0900

    0523

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Logo from './components/Logo/Logo';
 import './App.css';
-import { Route } from 'react-router';
+import { Route, useHistory } from 'react-router';
 import Youtuber from './pages/Main/Youtuber/Youtuber';
 import Editer from './pages/Main/Editer/Editer';
 import Thumbnailer from './pages/Main/Thumbnailer/Thumbnailer';
@@ -32,7 +31,7 @@ import { userLogout } from './redux/redux-login/loginReducer';
 import Chat from './pages/Main/components/Chat/Chat';
 import EDetail from './pages/Main/Editer/EDetail';
 import ResetPassword from './components/Login-SignUp/Login/ResetPassword';
-import { ToastCenter } from './modules/ToastModule';
+import { ToastAlert, ToastCenter } from './modules/ToastModule';
 import WModify from './pages/Main/Winwin/WModify';
 import ThumbRegister from './pages/Main/Thumbnailer/ThumbRegister';
 import ThumbDetail from './pages/Main/Thumbnailer/ThumbDetail';
@@ -43,57 +42,10 @@ import InfoModify from './components/InfoModify/InfoModify';
 import EboardModify from './pages/Main/Editer/EboardModify';
 import ThumbModify from './pages/Main/Thumbnailer/ThumbModify';
 import SignOut from './components/SignOut/SignOut';
-import { getNotificationsData } from './redux/notification/notifiReducer';
-=======
-import React, { useEffect, useState } from "react";
-import Logo from "./components/Logo/Logo";
-import "./App.css";
-import { Route, useHistory } from "react-router";
-import Youtuber from "./pages/Main/Youtuber/Youtuber";
-import Editer from "./pages/Main/Editer/Editer";
-import Thumbnailer from "./pages/Main/Thumbnailer/Thumbnailer";
-import Winwin from "./pages/Main/Winwin/Winwin";
-import Help from "./pages/Main/Help/Help";
-import Ydetail from "./pages/Main/Youtuber/Ydetail";
-import SignUp1 from "./components/Login-SignUp/SignUp/SignUp1";
-import { withRouter } from "react-router";
-import Navi from "./components/Navi/Navi";
-import YoutuberProfile from "./pages/Profile/YoutuberProfile";
-import Yregister from "./pages/Main/Youtuber/Yregister";
-import Switch from "react-bootstrap/Switch";
-import YmodifyTest from "./pages/Main/Youtuber/YmodifyTest";
-import { useLocation } from "react-router-dom";
-import MainWrapper from "./MainWrapper";
-import PageNotFound from "./pages/Error/PageNotFound";
-import Footer from "./components/Footer";
-import FindPassword from "./components/Login-SignUp/Login/FindPassword";
-import Wdetail from "./pages/Main/Winwin/Wdetail";
-import Wregister from "./pages/Main/Winwin/Wregister";
-import EditorRegister from "./pages/Main/Editer/EditorRegister";
-import { useDispatch, useSelector } from "react-redux";
-import { getLoaded, getLoading } from "./redux/loading/loadingReducer";
-import Loader from "./components/Loading/Loader";
-import instance from "./AxiosConfig";
-import { userLogout } from "./redux/redux-login/loginReducer";
-import Chat from "./pages/Main/components/Chat/Chat";
-import EDetail from "./pages/Main/Editer/EDetail";
-import ResetPassword from "./components/Login-SignUp/Login/ResetPassword";
-import { ToastAlert, ToastCenter } from "./modules/ToastModule";
-import WModify from "./pages/Main/Winwin/WModify";
-import ThumbRegister from "./pages/Main/Thumbnailer/ThumbRegister";
-import ThumbDetail from "./pages/Main/Thumbnailer/ThumbDetail";
-import BeforeModify from "./components/InfoModify/BeforeModify";
-import Admin_main from "./pages/Admin/Admin_main";
-import PasswordModify from "./components/InfoModify/PasswordModify";
-import InfoModify from "./components/InfoModify/InfoModify";
-import EboardModify from "./pages/Main/Editer/EboardModify";
-import ThumbModify from "./pages/Main/Thumbnailer/ThumbModify";
-import SignOut from "./components/SignOut/SignOut";
-import { deleteNotifications } from "./apiService/MainApiService";
-import ChatModal from "./pages/Main/components/Chat/ChatModal";
-import { AiFillWechat } from "react-icons/ai";
-import { toastWithPush } from "./modules/ToastWithPush";
->>>>>>> 1e1b7053b82149d775aa73ad41396da82dcde809
+import { deleteNotifications } from './apiService/MainApiService';
+import ChatModal from './pages/Main/components/Chat/ChatModal';
+import { AiFillWechat } from 'react-icons/ai';
+import { toastWithPush } from './modules/ToastWithPush';
 /* Logo 컴포넌트 제외할 페이지들 담아놓은 배열 */
 const exceptArray = ['/SignUp1', '/SignUp1/Required', '/SignUp1/NonRequired'];
 
@@ -115,7 +67,9 @@ function App() {
 
   /* 로딩 */
   const dispatch = useDispatch();
-  const { loading, notificationData } = useSelector((state) => state.loadingReducer);
+  const { loading, notificationData } = useSelector(
+    (state) => state.loadingReducer
+  );
   const { userData } = useSelector((state) => state.loginReducer);
   useEffect(() => {
     instance.interceptors.request.use(
@@ -155,9 +109,14 @@ function App() {
   /* 로딩 끝 */
   //알림
   useEffect(() => {
-    if (notificationData.length > 0 && notificationData[0].notiId !== 0 && userData && userData.id !== 0) {
+    if (
+      notificationData.length > 0 &&
+      notificationData[0].notiId !== 0 &&
+      userData &&
+      userData.id !== 0
+    ) {
       notificationData.forEach((notification) => {
-        if (notification.type === "commentNoti") {
+        if (notification.type === 'commentNoti') {
           ToastAlert(() =>
             toastWithPush(
               `${notification.resipeint.nickname}님께서 ${notification.comment.board.title}글에 댓글을 남기셨습니다.`,
@@ -178,19 +137,20 @@ function App() {
     <div>
       {userData && userData.id !== 0 && (
         <>
-          <AiFillWechat className='chat_button' onClick={() => setModalIsOpen(true)} />
-          {modalIsOpen && <ChatModal modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen} />}
+          <AiFillWechat
+            className='chat_button'
+            onClick={() => setModalIsOpen(true)}
+          />
+          {modalIsOpen && (
+            <ChatModal
+              modalIsOpen={modalIsOpen}
+              setModalIsOpen={setModalIsOpen}
+            />
+          )}
         </>
       )}
       {exceptArray.indexOf(location.pathname) < 0 && <Navi />}
       {exceptArray.indexOf(location.pathname) < 0 && <Logo />}
-<<<<<<< HEAD
-      {console.log('전페이지', prevLocation)}
-      {exceptArray.includes(prevLocation) === true && location.pathname === '/'
-        ? console.log('회원가입에서 왔군')
-        : console.log('그냥 왔군')}
-=======
->>>>>>> 1e1b7053b82149d775aa73ad41396da82dcde809
       <div>
         {loading && <Loader type='spin' color='#ff9411' />}
         <Switch>
