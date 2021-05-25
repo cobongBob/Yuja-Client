9f805841b9ea187451e01ad2da58a27ccc738b6a
kiteh <kiteho8962@naver.com>
2021-05-25 16:07:35 +0900

    Yeon routeif 수정

--- src/routerif/RouteIf.js
+++ src/routerif/RouteIf.js
@@ -1,33 +1,45 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Route, useHistory } from 'react-router-dom';
+import { ToastCenter } from '../modules/ToastModule';
 import PageNotFound from '../pages/Error/PageNotFound';
 
 const RouteIf = ({ authorities, roles, component: Component, ...rest }) => {
+  const { userData } = useSelector((state) => state.loginReducer);
+  const history = useHistory();
+
   return (
     <Route
       {...rest}
       render={(props) => {
-        let isValidate = false;
-        if (authorities.length !== 0) {
-          authorities.forEach((authority) => {
-            if (!roles.includes(authority)) {
-              isValidate = true;
-              return false;
-            }
-          });
+        if (userData && userData.id > 0) {
+          let isValidate = false;
+          if (authorities && authorities.length !== 0) {
+            authorities.forEach((authority) => {
+              if (roles && !roles.includes(authority)) {
+                isValidate = true;
+                return false;
+              }
+            });
+          } else {
+            isValidate = true;
+          }
+
+          if (isValidate) {
+            return <PageNotFound />;
+          }
+
+          if (Component) {
+            return <Component {...props} role={authorities} />;
+          }
+
+          return null;
         } else {
-          isValidate = true;
+          history.push('/');
+          ToastCenter('로그인 해주세요!');
+          // 로그인 모달을 열도록...?
+          // 주소를 직접 치고 들어가면 로그인해주세요...?
         }
-
-        if (isValidate) {
-          return <PageNotFound />;
-        }
-
-        if (Component) {
-          return <Component {...props} role={authorities} />;
-        }
-
-        return null;
       }}></Route>
   );
 };
