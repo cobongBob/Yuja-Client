e083bc58a8f8fac15d23d4a661997a95563e2719
YeonHo <kiteho8962@naver.com>
2021-06-04 15:15:54 +0900

    Yeon slide infinite

--- src/pages/VideoBox/VideoBox.js
+++ src/pages/VideoBox/VideoBox.js
@@ -1,10 +1,7 @@
-import React, { useMemo } from 'react';
 import './VideoBox.scss';
 import Evideo from '../../components/Slide/Evideo';
 import Svideo from '../../components/Slide/Svideo';
-import PrevArrow from '../../components/Slide/PrevArrow';
-import NextArrow from '../../components/Slide/NextArrow';
-import { useSelector } from 'react-redux';
+
 const VideoBox = () => {
   return (
     <div className="VideoBox_Wrapper">
