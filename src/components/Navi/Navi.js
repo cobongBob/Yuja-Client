b683b8e579bf4afa2e0c81c13b0dd310215b9fe4
Jongmin <dbswww@naver.com>
2021-06-07 09:31:06 +0900

    react-charts-js dependency added

--- src/components/Navi/Navi.js
+++ src/components/Navi/Navi.js
@@ -1,118 +1,79 @@
-import React from 'react';
-import './Navi.scss';
-import { Link, useLocation } from 'react-router-dom';
-import './SignButton.scss';
-import LoginModal from '../Login-SignUp/Login/LoginModal';
-import { useSelector } from 'react-redux';
-import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import React from "react";
+import "./Navi.scss";
+import { Link, useLocation } from "react-router-dom";
+import "./SignButton.scss";
+import LoginModal from "../Login-SignUp/Login/LoginModal";
+import { useSelector } from "react-redux";
+import { Nav, Navbar } from "react-bootstrap";
 
 const Navi = ({ allNotifications, setModalIsOpen }) => {
   const pathname = useLocation().pathname;
   const { authorities } = useSelector((state) => state.loginReducer);
   return (
-    <div className="nav">
-      <Navbar className="NavWrapper" collapseOnSelect bg="light" expand="lg">
+    <div className='nav'>
+      <Navbar className='NavWrapper' collapseOnSelect bg='light' expand='lg'>
         <Navbar.Brand>
-          <LoginModal
-            allNotifications={allNotifications}
-            setModalIsOpen={setModalIsOpen}
-          />
+          <LoginModal allNotifications={allNotifications} setModalIsOpen={setModalIsOpen} />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#">
-              <Link to="/">
-                <li
-                  className={
-                    pathname === '/' ? 'nav-link-disabled' : 'nav-link'
-                  }
-                >
-                  메인
-                </li>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='mr-auto'>
+            <Nav.Link href='#'>
+              <Link to='/'>
+                <li className={pathname === "/" ? "nav-link-disabled" : "nav-link"}>메인</li>
               </Link>
             </Nav.Link>
-            <Nav.Link href="#">
-              <Link to="/Youtuber/1">
+            <Nav.Link href='#'>
+              <Link to='/Youtuber/1'>
                 <li
                   className={
-                    pathname.includes('/Youtuber') ||
-                    pathname.includes('/Ydetail') ||
-                    pathname.includes('/YoutuberRegister') ||
-                    pathname.includes('/YboardModify')
-                      ? 'nav-link-disabled'
-                      : 'nav-link'
+                    pathname.includes("/Youtuber") ||
+                    pathname.includes("/Ydetail") ||
+                    pathname.includes("/YoutuberRegister") ||
+                    pathname.includes("/YboardModify")
+                      ? "nav-link-disabled"
+                      : "nav-link"
                   }
                 >
                   유튜버
                 </li>
               </Link>
             </Nav.Link>
-            <Nav.Link href="#">
-              <Link to="/Eboard/Editor/1">
-                <li
-                  className={
-                    pathname.includes('/Editor')
-                      ? 'nav-link-disabled'
-                      : 'nav-link'
-                  }
-                >
-                  편집자
-                </li>
+            <Nav.Link href='#'>
+              <Link to='/Eboard/Editor/1'>
+                <li className={pathname.includes("/Editor") ? "nav-link-disabled" : "nav-link"}>편집자</li>
               </Link>
             </Nav.Link>
-            <Nav.Link href="#">
-              <Link to="/Thboard/Thumb/1">
-                <li
-                  className={
-                    pathname.includes('/Thumb')
-                      ? 'nav-link-disabled'
-                      : 'nav-link'
-                  }
-                >
-                  썸네일러
-                </li>
+            <Nav.Link href='#'>
+              <Link to='/Thboard/Thumb/1'>
+                <li className={pathname.includes("/Thumb") ? "nav-link-disabled" : "nav-link"}>썸네일러</li>
               </Link>
             </Nav.Link>
-            <Nav.Link href="#">
-              <Link to="/Community/Winwin/1">
+            <Nav.Link href='#'>
+              <Link to='/Community/Winwin/1'>
                 <li
                   className={
-                    pathname.includes('/Community') ||
-                    pathname.includes('/BoardDetail') ||
-                    pathname.includes('/BoardModify')
-                      ? 'nav-link-disabled'
-                      : 'nav-link'
+                    pathname.includes("/Community") ||
+                    pathname.includes("/BoardDetail") ||
+                    pathname.includes("/BoardModify")
+                      ? "nav-link-disabled"
+                      : "nav-link"
                   }
                 >
                   커뮤니티
                 </li>
               </Link>
             </Nav.Link>
-            <Nav.Link href="#">
-              <Link to="/Help">
-                <li
-                  className={
-                    pathname === '/Help' ? 'nav-link-disabled' : 'nav-link'
-                  }
-                >
-                  고객센터
-                </li>
+            <Nav.Link href='#'>
+              <Link to='/Help'>
+                <li className={pathname === "/Help" ? "nav-link-disabled" : "nav-link"}>고객센터</li>
               </Link>
             </Nav.Link>
-            <Nav.Link href="#">
-              {authorities && authorities.includes('ADMIN') && (
+            <Nav.Link href='#'>
+              {authorities && authorities.includes("ADMIN") && (
                 <>
-                  <Link to="/Admin/AdminUsers">
-                    <li
-                      className={
-                        pathname.includes('/Admin')
-                          ? 'nav-link-disabled'
-                          : 'nav-link'
-                      }
-                    >
-                      관리페이지
-                    </li>
+                  <Link to='/Admin/AdminUsers'>
+                    <li className={pathname.includes("/Admin") ? "nav-link-disabled" : "nav-link"}>관리페이지</li>
                   </Link>
                 </>
               )}
