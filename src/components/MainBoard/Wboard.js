dea2af5c6a0af25a91e5defe5dcc2b95829cb986
Jongmin <dbswww@naver.com>
2021-06-07 15:30:58 +0900

    About account responsive css done

--- src/components/MainBoard/Wboard.js
+++ src/components/MainBoard/Wboard.js
@@ -1,59 +1,62 @@
-import React from 'react';
-import './Wboard.scss';
-import { Col, Row } from 'react-bootstrap';
-import { FaHandshake } from 'react-icons/fa';
-import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router';
+import React from "react";
+import "./Wboard.scss";
+import { Col, Row } from "react-bootstrap";
+import { FaHandshake } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router";
 const Wboard = () => {
   const { WmainList, CmainList } = useSelector((state) => state.mainReducer);
   const history = useHistory();
   return (
     <div>
-      <div
-        onClick={() => history.push('/Community/Winwin/1')}
-        className="win-title"
-      >
+      <div onClick={() => history.push("/Community/Winwin/1")} className='win-title'>
         <span>
           <FaHandshake></FaHandshake>
-        </span>{' '}
-        Win-Win{' '}
+        </span>{" "}
+        Win-Win{" "}
       </div>
-      <Row className="win-body">
+      <Row className='win-body'>
         <Col sm>
-          <div className="WinWrapper">
-            <div className="win-grow">성장해요</div>
-            <div className="WinListWrapper">
-              <div className="WinList">
-                <table className="ListTable">
+          <div className='WinWrapper'>
+            <div className='win-grow'>
+              <span
+                style={{ cursor: "pointer" }}
+                onClick={() => {
+                  history.push("/Community/Winwin/1");
+                }}
+              >
+                성장해요
+              </span>
+            </div>
+            <div className='WinListWrapper'>
+              <div className='WinList'>
+                <table className='ListTable'>
                   <thead>
                     <tr>
-                      <td className="MainWriter">작성자</td>
-                      <td>제목</td>
-                      <td>날짜</td>
-                      <td>조회수</td>
+                      <td className='MainWriter'>작성자</td>
+                      <td className='MainTitle'>제목</td>
+                      <td className='MainDate'>날짜</td>
+                      <td className='MainHit'>조회수</td>
                     </tr>
                   </thead>
                   <tbody>
-                    {WmainList &&
-                      WmainList.map((list, index) => (
-                        <tr key={index}>
-                          <td>{list.user.nickname}</td>
-                          <td
-                            onClick={() =>
-                              history.push(`/BoardDetail/Winwin/${list.id}/1`)
-                            }
-                            style={{ cursor: 'pointer' }}
-                          >
-                            {list.title}
-                            <span className="commentNum">
-                              {' '}
-                              [{list.comments}]{' '}
-                            </span>
-                          </td>
-                          <td>{list.updatedDate.substr(0, 10)}</td>
-                          <td>{list.hit}</td>
-                        </tr>
-                      ))}
+                    {WmainList?.map((list, index) => (
+                      <tr
+                        key={index}
+                        onClick={() => {
+                          history.push(`/BoardDetail/Winwin/${list.id}/1`);
+                        }}
+                        style={{ cursor: "pointer" }}
+                      >
+                        <td>{list.user.nickname}</td>
+                        <td>
+                          {list.title}
+                          <span className='commentNum'> [{list.comments}] </span>
+                        </td>
+                        <td>{list.createDate.substr(5, 5)}</td>
+                        <td>{list.hit}</td>
+                      </tr>
+                    ))}
                   </tbody>
                 </table>
               </div>
@@ -61,40 +64,44 @@ const Wboard = () => {
           </div>
         </Col>
         <Col sm>
-          <div className="WinWrapper">
-            <div className="win-collabo">합방해요</div>
-            <div className="WinListWrapper">
-              <div className="WinList">
-                <table className="ListTable">
+          <div className='WinWrapper'>
+            <div className='win-collabo'>
+              <span
+                style={{ cursor: "pointer" }}
+                onClick={() => {
+                  history.push("/Community/Collabo/1");
+                }}
+              >
+                합방해요
+              </span>
+            </div>
+            <div className='WinListWrapper'>
+              <div className='WinList'>
+                <table className='ListTable'>
                   <thead>
                     <tr>
-                      <td className="MainWriter">작성자</td>
-                      <td>제목</td>
-                      <td className="MainDate">날짜</td>
-                      <td>조회수</td>
+                      <td className='MainWriter'>작성자</td>
+                      <td className='MainTitle'>제목</td>
+                      <td className='MainDate'>날짜</td>
+                      <td className='MainHit'>조회수</td>
                     </tr>
                   </thead>
                   <tbody>
-                    {CmainList &&
-                      CmainList.map((list, index) => (
-                        <tr key={index}>
-                          <td>{list.user.nickname}</td>
-                          <td
-                            onClick={() =>
-                              history.push(`/BoardDetail/Collabo/${list.id}/1`)
-                            }
-                            style={{ cursor: 'pointer' }}
-                          >
-                            {list.title}
-                            <span className="commentNum">
-                              {' '}
-                              [{list.comments}]{' '}
-                            </span>
-                          </td>
-                          <td>{list.updatedDate.substr(0, 10)}</td>
-                          <td>{list.hit}</td>
-                        </tr>
-                      ))}
+                    {CmainList?.map((list, index) => (
+                      <tr
+                        key={index}
+                        onClick={() => history.push(`/BoardDetail/Collabo/${list.id}/1`)}
+                        style={{ cursor: "pointer" }}
+                      >
+                        <td>{list.user.nickname}</td>
+                        <td>
+                          {list.title}
+                          <span className='commentNum'> [{list.comments}] </span>
+                        </td>
+                        <td>{list.createDate.substr(5, 5)}</td>
+                        <td>{list.hit}</td>
+                      </tr>
+                    ))}
                   </tbody>
                 </table>
               </div>
