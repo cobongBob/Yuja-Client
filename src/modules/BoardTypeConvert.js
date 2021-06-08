f73fc233d690afb2afec0dbf5575a72c30f03911
Jongmin <dbswww@naver.com>
2021-06-08 14:27:15 +0900

    2021-06-08 merged before deploy

--- src/modules/BoardTypeConvert.js
+++ src/modules/BoardTypeConvert.js
@@ -54,7 +54,7 @@ export const BoardTypeConvertUrl = (reportedBoardCode, reportedBoardId) => {
   let boardUrl = "";
   const reportedBoardType = BoardTypeConvertReverse(reportedBoardCode);
   if (reportedBoardCode === 1) {
-    boardUrl = `/Ydetail/${reportedBoardId}`;
+    boardUrl = `/Ydetail/${reportedBoardId}/1`;
   } else if (reportedBoardCode === 2) {
     boardUrl = `/EDetail/Editor/${reportedBoardId}/1`;
   } else if (reportedBoardCode === 3) {
