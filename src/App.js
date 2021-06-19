fa8f2ad5cc699a56b14171096f16d7e40738f202
JongMin Yoon <Jongmin4943@gmail.com>
2021-06-19 15:29:50 +0900

    body css adjust, Time module added

--- src/App.js
+++ src/App.js
@@ -1,54 +1,54 @@
-import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
-import Logo from "./components/Logo/Logo";
-import "./App.css";
-import { Redirect, Route, useHistory } from "react-router";
-import Youtuber from "./pages/Youtuber/Youtuber";
-import Editor from "./pages/Editor/Editor";
-import Thumbnailer from "./pages/Thumbnailer/Thumbnailer";
-import Winwin from "./pages/Community/Winwin";
-import Help from "./pages/Help/Help";
-import Ydetail from "./pages/Youtuber/Ydetail";
-import SignUp from "./components/Login-SignUp/SignUp/SignUp";
-import { withRouter } from "react-router";
-import Navi from "./components/Navi/Navi";
-import Yregister from "./pages/Youtuber/Yregister";
-import Switch from "react-bootstrap/Switch";
-import YmodifyTest from "./pages/Youtuber/YmodifyTest";
-import { useLocation } from "react-router-dom";
-import MainWrapper from "./MainWrapper";
-import Footer from "./components/Footer";
-import FindPassword from "./components/Login-SignUp/Login/FindPassword";
-import Wdetail from "./pages/Community/Wdetail";
-import Wregister from "./pages/Community/Wregister";
-import EditorRegister from "./pages/Editor/EditorRegister";
-import { useDispatch, useSelector } from "react-redux";
-import { getLoaded, getLoading } from "./redux/loading/loadingReducer";
-import Loader from "./components/Loading/Loader";
-import instance from "./AxiosConfig";
-import { addAuthority, delAuthority, userLogout } from "./redux/redux-login/loginReducer";
-import EDetail from "./pages/Editor/EDetail";
-import ResetPassword from "./components/Login-SignUp/Login/ResetPassword";
-import { ToastAlert, ToastCenter, ToastAlertNoDupl } from "./modules/ToastModule";
-import WModify from "./pages/Community/WModify";
-import ThumbRegister from "./pages/Thumbnailer/ThumbRegister";
-import ThumbDetail from "./pages/Thumbnailer/ThumbDetail";
-import BeforeModify from "./components/InfoModify/BeforeModify";
-import Admin_main from "./pages/Admin/Admin_main";
-import PasswordModify from "./components/InfoModify/PasswordModify";
-import InfoModify from "./components/InfoModify/InfoModify";
-import EboardModify from "./pages/Editor/EboardModify";
-import ThumbModify from "./pages/Thumbnailer/ThumbModify";
-import SignOut from "./components/SignOut/SignOut";
-import { deleteNotifications } from "./apiService/MainApiService";
-import { RiChat1Fill } from "react-icons/ri";
-import { toastWithPush } from "./modules/ToastWithPush";
-import YoutuberRequest from "./components/InfoModify/YoutuberRequest";
-import { getAllNotifications } from "./redux/loading/notiReducer";
-import RouteIf from "./routerif/RouteIf";
-import ChatFrame from "./components/NewChat/ChatFrame";
-import MyPage from "./pages/MyPage/MyPage";
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import Logo from './components/Logo/Logo';
+import './App.css';
+import { Redirect, Route, useHistory } from 'react-router';
+import Youtuber from './pages/Youtuber/Youtuber';
+import Editor from './pages/Editor/Editor';
+import Thumbnailer from './pages/Thumbnailer/Thumbnailer';
+import Winwin from './pages/Community/Winwin';
+import Help from './pages/Help/Help';
+import Ydetail from './pages/Youtuber/Ydetail';
+import SignUp from './components/Login-SignUp/SignUp/SignUp';
+import { withRouter } from 'react-router';
+import Navi from './components/Navi/Navi';
+import Yregister from './pages/Youtuber/Yregister';
+import Switch from 'react-bootstrap/Switch';
+import YmodifyTest from './pages/Youtuber/YmodifyTest';
+import { useLocation } from 'react-router-dom';
+import MainWrapper from './MainWrapper';
+import Footer from './components/Footer';
+import FindPassword from './components/Login-SignUp/Login/FindPassword';
+import Wdetail from './pages/Community/Wdetail';
+import Wregister from './pages/Community/Wregister';
+import EditorRegister from './pages/Editor/EditorRegister';
+import { useDispatch, useSelector } from 'react-redux';
+import { getLoaded, getLoading } from './redux/loading/loadingReducer';
+import Loader from './components/Loading/Loader';
+import instance from './AxiosConfig';
+import { addAuthority, delAuthority, userLogout } from './redux/redux-login/loginReducer';
+import EDetail from './pages/Editor/EDetail';
+import ResetPassword from './components/Login-SignUp/Login/ResetPassword';
+import { ToastAlert, ToastCenter, ToastAlertNoDupl } from './modules/ToastModule';
+import WModify from './pages/Community/WModify';
+import ThumbRegister from './pages/Thumbnailer/ThumbRegister';
+import ThumbDetail from './pages/Thumbnailer/ThumbDetail';
+import BeforeModify from './components/InfoModify/BeforeModify';
+import Admin_main from './pages/Admin/Admin_main';
+import PasswordModify from './components/InfoModify/PasswordModify';
+import InfoModify from './components/InfoModify/InfoModify';
+import EboardModify from './pages/Editor/EboardModify';
+import ThumbModify from './pages/Thumbnailer/ThumbModify';
+import SignOut from './components/SignOut/SignOut';
+import { deleteNotifications } from './apiService/MainApiService';
+import { RiChat1Fill } from 'react-icons/ri';
+import { toastWithPush } from './modules/ToastWithPush';
+import YoutuberRequest from './components/InfoModify/YoutuberRequest';
+import { getAllNotifications } from './redux/loading/notiReducer';
+import RouteIf from './routerif/RouteIf';
+import ChatFrame from './components/NewChat/ChatFrame';
+import MyPage from './pages/MyPage/MyPage';
 /* Logo 컴포넌트 제외할 페이지들 담아놓은 배열 */
-const exceptArray = ["/SignUp", "/SignUp/Required", "/SignUp/NonRequired"];
+const exceptArray = ['/SignUp', '/SignUp/Required', '/SignUp/NonRequired'];
 
 function App() {
   const location = useLocation();
@@ -97,16 +97,22 @@ function App() {
       (error) => {
         //실패시 로딩창 종료
         if (error.response && error.response.data) {
-          if (error.response.data.message && error.response.data.message.startsWith("해당글 없음")) {
-            history.push("/");
+          if (
+            error.response.data.message &&
+            error.response.data.message.startsWith('해당글 없음')
+          ) {
+            history.push('/');
           } else if (
             error.response.data.message &&
-            error.response.data.message.startsWith("이용이 정지된 계정입니다.")
+            error.response.data.message.startsWith('이용이 정지된 계정입니다.')
           ) {
             userLogout().then((res) => {
               dispatch(res);
             });
-          } else if (error.response.data.message && error.response.data.message.startsWith("로그인 해주세요")) {
+          } else if (
+            error.response.data.message &&
+            error.response.data.message.startsWith('로그인 해주세요')
+          ) {
             userLogout().then((res) => {
               dispatch(res);
             });
@@ -125,18 +131,22 @@ function App() {
 
   //알림
   useEffect(() => {
-    if (notificationData.length > 0 && notificationData[0].notiId !== 0 && userData && userData.id !== 0) {
+    if (
+      notificationData.length > 0 &&
+      notificationData[0].notiId !== 0 &&
+      userData &&
+      userData.id !== 0
+    ) {
       notificationData.forEach((notification) => {
-        if (notification.type === "commentNoti" && notification.resipeint.id === userData.id) {
+        if (notification.type === 'commentNoti' && notification.resipeint.id === userData.id) {
           ToastAlert(() =>
             toastWithPush(
               <ul
                 style={{
-                  listStyle: "none",
-                  paddingLeft: "0",
-                  marginBottom: "0",
-                }}
-              >
+                  listStyle: 'none',
+                  paddingLeft: '0',
+                  marginBottom: '0',
+                }}>
                 <li>{`${notification.comment.board.title}`}</li>
                 <li>글에 댓글이 달렸습니다.</li>
               </ul>,
@@ -144,7 +154,10 @@ function App() {
               history
             )
           );
-        } else if (notification.type === "nestedComment" && notification.resipeint.id === userData.id) {
+        } else if (
+          notification.type === 'nestedComment' &&
+          notification.resipeint.id === userData.id
+        ) {
           ToastAlert(() =>
             toastWithPush(
               `${notification.sender.nickname}님께서 ${notification.comment.board.title}글의 댓글에 답글을 남기셨습니다.`,
@@ -152,31 +165,43 @@ function App() {
               history
             )
           );
-        } else if (notification.type === "chatNoti" && notification.resipeint.id === userData.id) {
+        } else if (notification.type === 'chatNoti' && notification.resipeint.id === userData.id) {
           ToastAlertNoDupl(`${notification.sender.nickname}님으로부터 새로운 채팅이 있습니다.`);
-        } else if (notification.type === "editNoti" && notification.resipeint.id === userData.id) {
+        } else if (notification.type === 'editNoti' && notification.resipeint.id === userData.id) {
           ToastAlertNoDupl(`에디터로 등록되셨습니다.`);
-          addAuthority("EDITOR").then((res) => {
+          addAuthority('EDITOR').then((res) => {
             dispatch(res);
           });
-        } else if (notification.type === "thumbNoti" && notification.resipeint.id === userData.id) {
+        } else if (notification.type === 'thumbNoti' && notification.resipeint.id === userData.id) {
           ToastAlertNoDupl(`썸네일러로 등록되셨습니다.`);
-          addAuthority("THUMBNAILER").then((res) => {
+          addAuthority('THUMBNAILER').then((res) => {
             dispatch(res);
           });
-        } else if (notification.type === "youtubeNoti" && notification.resipeint.id === userData.id) {
+        } else if (
+          notification.type === 'youtubeNoti' &&
+          notification.resipeint.id === userData.id
+        ) {
           ToastAlertNoDupl(`유튜버로 등록되셨습니다.`);
-          addAuthority("YOUTUBER").then((res) => {
+          addAuthority('YOUTUBER').then((res) => {
             dispatch(res);
           });
-        } else if (notification.type === "rejectNoti" && notification.resipeint.id === userData.id) {
+        } else if (
+          notification.type === 'rejectNoti' &&
+          notification.resipeint.id === userData.id
+        ) {
           ToastAlertNoDupl(`유튜버로 등록이 거절되었습니다. 신청 절차를 다시 확인해주세요.`);
-        } else if (notification.type === "editDelNoti" && notification.resipeint.id === userData.id) {
+        } else if (
+          notification.type === 'editDelNoti' &&
+          notification.resipeint.id === userData.id
+        ) {
           ToastAlert(`편집자 등록이 해제 되었습니다.`);
           delAuthority(`EDITOR`).then((res) => {
             dispatch(res);
           });
-        } else if (notification.type === "thumbDelNoti" && notification.resipeint.id === userData.id) {
+        } else if (
+          notification.type === 'thumbDelNoti' &&
+          notification.resipeint.id === userData.id
+        ) {
           ToastAlert(`썸네일러 등록이 해제 되었습니다.`);
           delAuthority(`THUMBNAILER`).then((res) => {
             dispatch(res);
@@ -210,7 +235,7 @@ function App() {
   }, []);
   useEffect(() => {
     if (userData && userData.id > 0) {
-      const events = ["load", "click"];
+      const events = ['load', 'click'];
       const resetTimeout = () => {
         clearTimeouts();
         setTimeouts();
@@ -229,7 +254,10 @@ function App() {
   }, [clearTimeouts, setTimeouts, userData]);
   //자동로그아웃 끝
 
-  const role = useMemo(() => ["STRANGER", "GENERAL", "YOUTUBER", "EDITOR", "THUMBNAILER", "MANAGER", "ADMIN"], []);
+  const role = useMemo(
+    () => ['STRANGER', 'GENERAL', 'YOUTUBER', 'EDITOR', 'THUMBNAILER', 'MANAGER', 'ADMIN'],
+    []
+  );
 
   window.onkeydown = useCallback((e) => {
     if (e.keyCode === 27) {
@@ -253,21 +281,27 @@ function App() {
         <>
           <RiChat1Fill
             className={
-              allNotifications && allNotifications.find((noti) => noti.type === "chatNoti")
-                ? "chat_button_noti"
-                : "chat_button"
+              allNotifications && allNotifications.find((noti) => noti.type === 'chatNoti')
+                ? 'chat_button_noti'
+                : 'chat_button'
             }
-            onClick={modalIsOpen === false ? () => setModalIsOpen(true) : () => setModalIsOpen(false)}
+            onClick={
+              modalIsOpen === false ? () => setModalIsOpen(true) : () => setModalIsOpen(false)
+            }
           />
 
-          {modalIsOpen === true ? <ChatFrame modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen} /> : ""}
+          {modalIsOpen === true ? (
+            <ChatFrame modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen} />
+          ) : (
+            ''
+          )}
         </>
       )}
       {exceptArray.indexOf(location.pathname) < 0 && (
         <Navi allNotifications={allNotifications} setModalIsOpen={setModalIsOpen} />
       )}
       {exceptArray.indexOf(location.pathname) < 0 && <Logo />}
-      <div>
+      <div style={{ minHeight: '550px' }}>
         {loading && <Loader type='spin' color='#ff9411' />}
         <Switch>
           <Route exact path='/' component={MainWrapper} />
